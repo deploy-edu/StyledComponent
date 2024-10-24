edb0d43cdf9f0d68ae12acceff523689659c4c62
Juwon Jeong <2jw0718@gmail.com>
2024-10-24 11:23:21 +0900

    theme provider 추가

--- App.tsx
+++ App.tsx
@@ -1,25 +1,32 @@
 import styled from '@emotion/native';
+import { ThemeProvider } from '@emotion/react';
 import Button from './src/components/Button';
 import Input from './src/components/Input';
+import { lightTheme } from './src/constants/theme';
 
 const Container = styled.View`
   flex: 1;
   background-color: #fff;
   align-items: center;
   justify-content: center;
-  gap: 20;
+  gap: 20px;
 `;
 
 export default function App() {
   return (
-    <Container>
-      <Button title="Press me" onPress={() => console.log('Button pressed')} />
-      <Button
-        title="React Native"
-        onPress={() => console.log('React Native')}
-      />
-      <Input borderColor="#3498db" />
-      <Input borderColor="#9b59b6" />
-    </Container>
+    <ThemeProvider theme={lightTheme}>
+      <Container>
+        <Button
+          title="Press me"
+          onPress={() => console.log('Button pressed')}
+        />
+        <Button
+          title="React Native"
+          onPress={() => console.log('React Native')}
+        />
+        <Input borderColor="#3498db" />
+        <Input borderColor="#9b59b6" />
+      </Container>
+    </ThemeProvider>
   );
 }
