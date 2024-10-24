89bb89970066ff92ac41d3f7bf59319fb53eb0b2
Juwon Jeong <2jw0718@gmail.com>
2024-10-24 11:22:18 +0900

    테마 설정 추가

---
+++ src/constants/theme.ts
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  backgroundColor: '#fff',
+  text: '#000',
+  primary: '#3498db',
+  secondary: '#9b59b6',
+};
+
+export const darkTheme = {
+  backgroundColor: '#1e1e1e',
+  text: '#e0e0e0',
+  primary: '#2980b9',
+  secondary: '#8e44ad',
+};
